9fbc30fe7addf6539eb1df0a356c6754c8f8a969
Aksha <akshaali01234@gmail.com>
2022-09-19 16:47:11 +0530

    Navigations and routing

--- pages/cats/[catId]/index.tsx
+++ pages/cats/[catId]/index.tsx
@@ -3,7 +3,18 @@ import { useRouter } from "next/router";
 const CatPost = () => {
   const router = useRouter();
   const catID = router.query.catId;
-  return <div>Cat Page {catID}</div>;
+
+  const handleDetailsClick = () => {
+    router.push(`/cats/${catID}//publicReview/${catID}`);
+  }
+
+  return (
+    <div>
+      <h1>Cat Page {catID}</h1>
+      <h2 onClick={handleDetailsClick}>Details of Cat</h2>
+      <h2>Public review of Cat</h2>
+    </div>
+  );
 };
 
 export default CatPost;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-     <h1>Blog about Cats</h1>
-     <h1>Cat picturesss</h1>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>
+          <h2>Blog about Cats</h2>
+        </a>
+      </Link>
+      <Link href="/cats">
+        <a>
+          <h2>Cat picturesss</h2>
+        </a>
+      </Link>
     </div>
   );
 };
