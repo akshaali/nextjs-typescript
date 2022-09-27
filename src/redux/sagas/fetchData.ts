7846f632991ea420a312a9528dfa065d429658f4
Aksha <akshaali01234@gmail.com>
2022-09-27 11:56:02 +0530

    fetch Data

--- src/redux/sagas/fetchData.ts
+++ src/redux/sagas/fetchData.ts
@@ -1,22 +1,18 @@
-import {takeLatest, call, put, all, select} from 'redux-saga/effects';
-import actionTypes from '../Constants';
-import {
-  saveData
-} from '../actions/fetchData';
+import { takeLatest, call, put, all, select } from "redux-saga/effects";
+import actionTypes from "../constants";
+import { saveData } from "../actions/fetchData";
 
-function* fetchDataSaga(args) {
-  try {
-    yield put(saveData[{name:"a"}])
-  } catch (e) {
-    if (__DEV__) {
-      console.log('ERROR post fetchDataSaga', e);
-    }
-  } finally {
-  }
+function* fetchDataSaga(args: unknown) {
+  // try {
+  //   yield put(saveData[{name:"a"}])
+  // } catch (e) {
+  //   if (__DEV__) {
+  //     console.log('ERROR post fetchDataSaga', e);
+  //   }
+  // } finally {
+  // }
 }
 
 export default function* Sagas() {
-  yield all([
-    takeLatest(actionTypes.FETCH_POST_DATA, fetchDataSaga),
-  ]);
+  yield all([takeLatest(actionTypes.FETCH_POST_DATA, fetchDataSaga)]);
 }
