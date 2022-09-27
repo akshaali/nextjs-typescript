f20ef1edd9c41992df92bbea8e32fbd86eb59687
Aksha <akshaali01234@gmail.com>
2022-09-27 13:29:34 +0530

    jest setup

--- __test__/greet.test.tsx
+++ __test__/greet.test.tsx
@@ -1,15 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import Greet from "@pages/greet";
 
-test("Greet renders coorectly", () => {
-  render(<Greet />);
-  const testElement = screen.getByText("Hello");
-  expect(testElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders coorectly", () => {
+    render(<Greet />);
+    const testElement = screen.getByText("Hello");
+    expect(testElement).toBeInTheDocument();
+  });
 
-test("Greet renders correctly with props", () => {
-  render(<Greet name={{ firstName: "Aksha", lastName: "Ali" }} />);
-  const testElement = screen.getByText("Hello Aksha");
-  expect(testElement).toBeInTheDocument();
+  test("renders correctly with props", () => {
+    render(<Greet name={{ firstName: "Aksha", lastName: "Ali" }} />);
+    const testElement = screen.getByText("Hello Aksha");
+    expect(testElement).toBeInTheDocument();
+  });
 });
